fa71229644ce4c5e1b31a1443766cd79912b755d
Jonti <kappalaie@hotmail.com>
2020-04-25 15:02:03 +0200

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,6 +8,7 @@ public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Problem https://codeforces.com/problemset/problem/984/A
 		Reader read = new Reader();
 		int[] n = new int[read.nextInt()];
 		for(int i=0;i<n.length;i++) {

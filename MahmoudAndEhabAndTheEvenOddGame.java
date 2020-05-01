f9dfacb55134d1fe5cca6c8ac7c4fc5213013d46
Jonti <kappalaie@hotmail.com>
2020-05-01 12:20:47 +0200

    Update MahmoudAndEhabAndTheEvenOddGame.java

--- MahmoudAndEhabAndTheEvenOddGame.java
+++ MahmoudAndEhabAndTheEvenOddGame.java
@@ -1,13 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class MahmoudAndEhabAndTheEvenOddGame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/959/A
-		Scanner scan = new Scanner(System.in);
+		// https://github.com/jontiboss
+		Reader scan = new Reader();
 		int n = scan.nextInt();
-		scan.close();
+		
 		//considering both players play optimally, the winner will be determine whether n is even or not.
 		if(n%2==0) {
 			System.out.println("Mahmoud");
@@ -17,5 +21,36 @@ public class MahmoudAndEhabAndTheEvenOddGame {
 			System.out.println("Ehab");
 		}
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
 }

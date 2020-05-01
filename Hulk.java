38e92b7deb66d6eaf0b027a18f658fbac9f20baa
Jonti <kappalaie@hotmail.com>
2020-05-01 12:25:58 +0200

    Update Hulk.java

--- Hulk.java
+++ Hulk.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Hulk {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/705/A
+		// https://github.com/jontiboss
 		int n;
 		boolean love = false;
 		StringBuilder sb = new StringBuilder();
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 		n = scan.nextInt();
-		scan.close();
 		for(int i = 0 ; i<n;i++) {
 			if(love == true) {
 				if(i==n-1) {
@@ -34,5 +37,36 @@ public class Hulk {
 		}
 		System.out.println(sb);
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
 }

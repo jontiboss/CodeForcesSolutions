410c32ca55defb40537a5d65d7b75e9dee29d1fa
Jonti <kappalaie@hotmail.com>
2020-05-01 12:33:28 +0200

    Update FafaAndHisCompany.java

--- FafaAndHisCompany.java
+++ FafaAndHisCompany.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class FafaAndHisCompany {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/935/A
+		// https://github.com/jontiboss
 		int combinations = 0;
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 		int employees = scan.nextInt();
-		scan.close();
+		
 		
 		//i is the number of leaders
 		for(int i = 1; i<employees;i++ ) {
@@ -25,5 +29,36 @@ public class FafaAndHisCompany {
 		}
 		System.out.println(combinations);
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
 }

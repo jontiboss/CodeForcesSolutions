8afcf86740aa2a04b3c33dcd18544c02d0ee1055
Jonti <kappalaie@hotmail.com>
2020-05-01 12:47:41 +0200

    Update Cards.java

--- Cards.java
+++ Cards.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Cards {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/1220/A
+		// https://github.com/jontiboss
 		int ones = 0,zeros = 0;
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 		// Not needed but is given as a input.
 		int n = scan.nextInt();
 		String word = scan.next();
-		scan.close();
 		byte[] wordsByte = word.getBytes();
 		//search for how many r and n there is in the array, which is equal to how many zeros or ones there is.
 		for(int i = 0; i<wordsByte.length;i++) {
@@ -31,5 +34,50 @@ public class Cards {
 		}
 		
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+		String nextLine() 
+		{ 
+			String str = ""; 
+			try
+			{ 
+				str = br.readLine(); 
+			} 
+			catch (IOException e) 
+			{ 
+				e.printStackTrace(); 
+			} 
+			return str; 
+		} 
+	}
+	
 }

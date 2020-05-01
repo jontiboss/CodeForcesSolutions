5232040f156fa1e76ebafc8294b5f6add4fcd7e7
Jonti <kappalaie@hotmail.com>
2020-05-01 12:29:27 +0200

    Update HitTheLottery.java

--- HitTheLottery.java
+++ HitTheLottery.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class HitTheLottery {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/996/A
+		// https://github.com/jontibosss
 		int money,papers = 0;
 
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 //		System.out.println("How much money do you have?");
 		money = scan.nextInt();
-		scan.close();
 		//Removes 100 as many times as possible and adds how many money papers u will get of this quantity.
 		if(money>=100) {
 			papers = papers +  money/100;
@@ -46,5 +49,36 @@ public class HitTheLottery {
 
 
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
 }

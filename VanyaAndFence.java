1c63edf907f02979193d08140266590580b5d181
Jonti <kappalaie@hotmail.com>
2020-05-01 12:01:53 +0200

    Update VanyaAndFence.java

--- VanyaAndFence.java
+++ VanyaAndFence.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class VanyaAndFence {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/677/A
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 	    int friends = scan.nextInt();
 	    int height = scan.nextInt();
 	    int width = 0;
@@ -20,8 +23,39 @@ public class VanyaAndFence {
 	    		width++;
 	    	}
 	    }
-	    scan.close();
 	    System.out.println(width);
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+
+	}
 }

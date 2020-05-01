a641655567e6548cb3b771ac404ece467fcc62fb
Jonti <kappalaie@hotmail.com>
2020-05-01 13:00:35 +0200

    Update AntonAndPolyhedrons.java

--- AntonAndPolyhedrons.java
+++ AntonAndPolyhedrons.java
@@ -1,17 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class AntonAndPolyhedrons {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/785/A
+		// https://github.com/jontiboss
 		int n,sides = 0;
 		String shape;
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 		//n is the number of polyhedrons in Anton's collection.
 //		System.out.println("How many shapes do you have?");
 		n = scan.nextInt();
-		shape = scan.nextLine();
 		for(int i = 0; i<n ;i++) {
 			//adds a new shape to the total
 //			System.out.println("Whats the name of the shape you want to add?");
@@ -38,8 +41,51 @@ public class AntonAndPolyhedrons {
 			}
 
 		}
-		scan.close();
 		System.out.println(sides);
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+		String nextLine() 
+		{ 
+			String str = ""; 
+			try
+			{ 
+				str = br.readLine(); 
+			} 
+			catch (IOException e) 
+			{ 
+				e.printStackTrace(); 
+			} 
+			return str; 
+		} 
+	}
 }

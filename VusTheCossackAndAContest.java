ac8960ca4c5ee0119c8d2b6e42047d602f4b9944
Jonti <kappalaie@hotmail.com>
2020-05-01 11:57:04 +0200

    Update VusTheCossackAndAContest.java

--- VusTheCossackAndAContest.java
+++ VusTheCossackAndAContest.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class VusTheCossackAndAContest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/contest/1186/problem/A
-		Scanner scan = new Scanner(System.in);
+		Reader scan = new Reader();
 		int n,m,k;
 		n = scan.nextInt();
 		m = scan.nextInt();
 		k = scan.nextInt();
-		scan.close();
 		// checks if Vus can give out both a pen and a notebook to all participants.
 		if(n<=m && n<=k) {
 			System.out.println("Yes");
@@ -19,5 +21,36 @@ public class VusTheCossackAndAContest {
 			System.out.println("No");
 		}
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
 }

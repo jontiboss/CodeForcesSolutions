5659e3cdeec35f319da6ac02e3139106440f305a
Jonti <kappalaie@hotmail.com>
2020-05-01 13:02:26 +0200

    Update AntonAndDanik.java

--- AntonAndDanik.java
+++ AntonAndDanik.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class AntonAndDanik {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/734/A
-		Scanner scan = new Scanner(System.in);
+		// https://github.com/jontiboss
+		Reader scan = new Reader();
 		int n = scan.nextInt();
 		int A=0,D=0;
-		String gamesequence = scan.next();
-		scan.close();
+		String gamesequence = scan.next();	
 		//Counts how many wins each player got
 		for(int i = 0;i<gamesequence.length();i++) {
 			if(gamesequence.charAt(i) == 'A') {
@@ -32,5 +35,38 @@ public class AntonAndDanik {
 		}
 
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
+
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+		}
+	
 
 }

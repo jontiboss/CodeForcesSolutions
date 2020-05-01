7fa5e168bc4fae59c8fe54e99569999f56c380fe
Jonti <kappalaie@hotmail.com>
2020-05-01 12:52:15 +0200

    Update BetweenTheOffices.java

--- BetweenTheOffices.java
+++ BetweenTheOffices.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BetweenTheOffices {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/867/A
-		Scanner scan = new Scanner(System.in);
+		// https://github.com/jontiboss
+		Reader scan = new Reader();
 		int f=0,s=0;
 		// Not used.
 		int n = scan.nextInt();
 		byte[] flights = scan.next().getBytes();
-		scan.close();
 		for(int i=1;i<flights.length;i++) {
 			
 			
@@ -29,6 +32,37 @@ public class BetweenTheOffices {
 				System.out.println("NO");
 			}
 		}
-	
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
+
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+	}
 
 }

7e594c76e2b2694515d885b88febd29f4aa0ff34
Jonti <kappalaie@hotmail.com>
2020-05-01 12:49:09 +0200

    Update Borze.java

--- Borze.java
+++ Borze.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Borze {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Problem https://codeforces.com/problemset/problem/32/B
-		Scanner scan = new Scanner(System.in);
+		// https://github.com/jontiboss
+		Reader scan = new Reader();
 		StringBuilder sb = new StringBuilder();
 		byte[] word = scan.next().getBytes();
-		scan.close();
 		for(int i = 0; i<word.length;i++) {
 			if(word[i] == 46) {
 				sb.append(0);
@@ -24,5 +27,37 @@ public class Borze {
 		}
 		System.out.println(sb.toString());
 	}
+	static class Reader 
+	{ 
+		BufferedReader br; 
+		StringTokenizer st; 
 
+		public Reader() 
+		{ 
+			br = new BufferedReader(new
+					InputStreamReader(System.in)); 
+		} 
+
+		String next() 
+		{ 
+			while (st == null || !st.hasMoreElements()) 
+			{ 
+				try
+				{ 
+					st = new StringTokenizer(br.readLine()); 
+				} 
+				catch (IOException e) 
+				{ 
+					e.printStackTrace(); 
+				} 
+			} 
+			return st.nextToken(); 
+		} 
+
+		int nextInt() 
+		{ 
+			return Integer.parseInt(next()); 
+		} 
+		}
+	
 }

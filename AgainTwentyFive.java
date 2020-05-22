3c1c4fb97228c9970657b28ec8ed85c139f4c1d2
MediumBrain <kappalaie@hotmail.com>
2020-05-22 11:59:37 +0200

    Add files via upload

---
+++ AgainTwentyFive.java
@@ -0,0 +1,14 @@
+
+
+public class AgainTwentyFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Problem http://codeforces.com/problemset/problem/630/A
+		// For more solutions check https://github.com/jontiboss
+		
+		// 5^n will always end with 25, so just print 25 for all the answers.
+		System.out.println(25);
+	}
+	
+}
